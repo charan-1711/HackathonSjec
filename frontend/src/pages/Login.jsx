7d67bab83bbcf3240b1512b4b662c86023578466
charan-1711 <charan.a1711@gmail.com>
2025-04-18 17:10:45 +0530

    Login page changes

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -33,12 +33,6 @@ export default function Login() {
   };
 
   return (
-<<<<<<< HEAD
-    <>
-      <h1>Login page</h1>
-      
-    </>
-=======
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="bg-white shadow-lg rounded-2xl p-8 w-96">
         <h1 className="text-2xl font-semibold text-gray-800 text-center mb-6">
@@ -89,6 +83,5 @@ export default function Login() {
         </div>
       </div>
     </div>
->>>>>>> c54069660d04e24981255f88bf7b0714c8a633cc
   );
 }
